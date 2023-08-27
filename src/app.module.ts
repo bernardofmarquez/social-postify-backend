6d9428a956e20aeeda543ffd2f8665945aeeeb07
Bernardo Marquez <marquez.cmbh@gmail.com>
2023-08-27 19:59:40 -0300

    build: inital commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MediasModule } from './medias/medias/medias.module';
+import { MediasModule } from './medias/medias.module';
+import { MediasModule } from './medias/medias/medias.module';
+
+@Module({
+  imports: [MediasModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
